ad014d3102280afcb74d4ad56f0830615f8a07a3
petertran98 <Petertran198@gmail.com>
2022-02-27 18:45:16 -0800

    Refactor hero data to be in a service and fetch hero data via service SYNCRONOUSLY

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/interfaces/hero';
 import { HEROES } from 'src/app/data/mock-heroes';
+import { HeroService } from 'src/app/services/hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,12 +10,19 @@ import { HEROES } from 'src/app/data/mock-heroes';
 export class HeroesComponent implements OnInit {
   //Means be a hero type | undefined
   selectedHero?: Hero;
-  heroes: Hero[] = HEROES;
+  heroes: Hero[] = [];
 
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
