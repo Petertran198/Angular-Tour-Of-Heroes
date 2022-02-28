d647ba2ae2012698407ff9b9b53e1367e012de28
petertran98 <Petertran198@gmail.com>
2022-02-27 16:55:37 -0800

    Loops, creating mock data, and added click event to HeroesComponent

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/interfaces/hero';
+import { HEROES } from 'src/app/data/mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = { id: 1, name: 'Windstorm' };
+  //Means be hero | undefined
+  selectedHero?: Hero;
+  heroes: Hero[] = HEROES;
+
   constructor() {}
 
   ngOnInit(): void {}
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
